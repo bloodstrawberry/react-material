3efa79b1921922fbb5aa35014ff95034e8db82ee
bloodstrawberry <vvv3334@hanmail.net>
2023-04-04 22:20:30 +0900

    add material ui

---
+++ src/page/Router1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Router1 = () => {
+    return (
+        <div>
+            r1
+        </div>
+    );
+};
+
+export default Router1;
---
+++ src/page/Router2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Router2 = () => {
+  return <div>r2</div>;
+};
+
+export default Router2;
