a5649081a4967d744c7755ebe7f95b3cc969564c
bloodstrawberry <vvv3334@hanmail.net>
2024-01-24 16:42:32 +0900

    Updates

---
+++ precache-manifest.6bd2278f794e6583b266b849f2761f55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "469e93cbbec7283f267b",
+    "url": "/react-material/static/css/main.f7e95713.chunk.css"
+  },
+  {
+    "revision": "469e93cbbec7283f267b",
+    "url": "/react-material/static/js/main.469e93cb.chunk.js"
+  },
+  {
+    "revision": "6fb3d34af06f52730e46",
+    "url": "/react-material/static/js/1.6fb3d34a.chunk.js"
+  },
+  {
+    "revision": "99a84c1e809c8289c066",
+    "url": "/react-material/static/css/2.468d7a4b.chunk.css"
+  },
+  {
+    "revision": "99a84c1e809c8289c066",
+    "url": "/react-material/static/js/2.99a84c1e.chunk.js"
+  },
+  {
+    "revision": "3e0863cbf3156f577c96",
+    "url": "/react-material/static/js/runtime~main.3e0863cb.js"
+  },
+  {
+    "revision": "f382e2f471b401384dd8f0ad62345c54",
+    "url": "/react-material/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-material/precache-manifest.ed76e9ca812ccf159493763fd72e1130.js"
+  "/react-material/precache-manifest.6bd2278f794e6583b266b849f2761f55.js"
 );
 
 workbox.clientsClaim();
